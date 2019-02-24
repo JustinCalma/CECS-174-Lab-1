6aa5f70cc8a57041884e08ecf8e541fc70b49b9c
Justin Calma <calma.justin.00@gmail.com>
2019-02-24 10:04:35 -0800

    Working script

---
+++ JustinCalma_Lab1.py
@@ -0,0 +1,6 @@
+className = input("The class name is: ")
+classLength = int(input("Enter the class length per week: "))
+studyTime = classLength * 2
+print("Expected study time outside of class is: ", studyTime)
+totalTime = classLength + 2*(classLength)
+print("Total expected study time for class is: ", totalTime)
